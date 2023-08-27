10f9e1cbff96b9b9256ce9e8cc9004789fa25d3c
Tomas Votruba <tomas.vot@gmail.com>
2023-08-27 19:02:27 +0200

    wip

---
+++ tests/Rector/RectorConfigPrinterTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TomasVotruba\SherlockTypes\Tests\Rector;
+
+use TomasVotruba\SherlockTypes\Tests\AbstractTestCase;
+
+final class RectorConfigPrinterTest extends AbstractTestCase
+{
+    // @todo test printing from phpstan result
+    // @todo here and there B-)
+}
