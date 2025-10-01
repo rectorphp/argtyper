267ee80b4c68930f61f85fabd5e5fe8e9551b188
Tomas Votruba <tomas.vot@gmail.com>
2025-10-01 16:00:29 +0200

    misc

--- tests/Rector/Fixture/expected-generated-rector.php
+++ tests/Rector/Fixture/expected-generated-rector.php
@@ -9,9 +9,6 @@ use Rector\TypeDeclaration\ValueObject\AddReturnTypeDeclaration;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->ruleWithConfiguration(AddReturnTypeDeclarationRector::class, [
         new AddReturnTypeDeclaration(SomeClass::class, 'someMethod', new PHPStan\Type\StringType()),
-        new AddReturnTypeDeclaration(SomeClass::class, 'anotherMethod', new \PHPStan\Type\ArrayType(
-            new \PHPStan\Type\MixedType(),
-            new \PHPStan\Type\MixedType()
-        )),
+        new AddReturnTypeDeclaration(SomeClass::class, 'anotherMethod', new \PHPStan\Type\ArrayType(new \PHPStan\Type\MixedType(), new PHPStan\Type\MixedType())),
     ]);
 };
