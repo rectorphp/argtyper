a85236d117b27feb3f7d04b790ddd15566a653e8
Tomas Votruba <tomas.vot@gmail.com>
2025-10-07 10:22:26 +0200

    fixup! fixup! fixup! add test

--- tests/Rector/Rector/AddParamIterableDocblockTypeRector/AddParamIterableDocblockTypeRectorTest.php
+++ tests/Rector/Rector/AddParamIterableDocblockTypeRector/AddParamIterableDocblockTypeRectorTest.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Rector\ArgTyper\Tests\Rector\Rector\AddParamIterableDocblockTypeRector;
 
+use Nette\Utils\FileSystem;
+use Nette\Utils\Json;
 use PHPUnit\Framework\Attributes\DataProvider;
+use Rector\ArgTyper\Enum\ConfigFilePath;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 
 final class AddParamIterableDocblockTypeRectorTest extends AbstractRectorTestCase
@@ -12,6 +15,22 @@ final class AddParamIterableDocblockTypeRectorTest extends AbstractRectorTestCas
     #[DataProvider('provideData')]
     public function test(string $filePath): void
     {
+        // 1. backup current phpstan dump
+        $phpstanCollectedDataFilePath = getcwd() . '/' . ConfigFilePath::phpstanCollectedData();
+
+        if (file_exists($phpstanCollectedDataFilePath)) {
+            FileSystem::copy($phpstanCollectedDataFilePath, $phpstanCollectedDataFilePath . '-temp');
+        }
+
+        // 2. create temp dump
+
+        Json::encode([
+
+        ]);
+
+
+        // 3.
+
         $this->doTestFile($filePath);
     }
 
