6e7d5b91924bb5d46f73dc714156fb939953e884
Tomas Votruba <tomas.vot@gmail.com>
2025-10-07 10:12:17 +0200

    add test

---
+++ tests/Rector/Rector/AddParamIterableDocblockTypeRector/AddParamIterableDocblockTypeRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\ArgTyper\Tests\Rector\Rector\AddParamIterableDocblockTypeRector;
+
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+final class AddParamIterableDocblockTypeRectorTest extends AbstractRectorTestCase
+{
+    public function provideConfigFilePath(): string
+    {
+    }
+}
