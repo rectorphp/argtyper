d43194f39b3dd381fc6a5e2df96cc50bd77dd5f2
Tomas Votruba <tomas.vot@gmail.com>
2025-10-15 21:03:11 +0200

    misc

--- bin/argtyper.php
+++ bin/argtyper.php
@@ -2,17 +2,5 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Input\ArgvInput;
-use Symfony\Component\Console\Output\ConsoleOutput;
+// run on project: bin/argtyper.php project
 
-require __DIR__ . '/../vendor/autoload.php';
-
-$containerFactory = new ContainerFactory();
-$container = $containerFactory->create();
-
-/** @var Application $application */
-$application = $container->make(Application::class);
-
-$exitCode = $application->run(new ArgvInput(), new ConsoleOutput());
-exit($exitCode);
