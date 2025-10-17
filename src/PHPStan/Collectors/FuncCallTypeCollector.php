9679eb4397f6843cd70ca709919b9cd8858d67e9
Tomas Votruba <tomas.vot@gmail.com>
2025-10-17 12:24:46 +0200

    fixup! add function like collector

--- src/PHPStan/Collectors/FuncCallTypeCollector.php
+++ src/PHPStan/Collectors/FuncCallTypeCollector.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Rector\ArgTyper\PHPStan\Collectors;
 
 use PhpParser\Node;
-use PhpParser\Node\Expr\CallLike;
 use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\FunctionLike;
 use PhpParser\Node\Identifier;
