a834b85ffdc1cdd2fab3177b2b60591fde40b388
Tomas Votruba <tomas.vot@gmail.com>
2025-11-21 13:29:53 +0000

    avoid scoping phpsparser and phpstan

--- scoper.php
+++ scoper.php
@@ -11,7 +11,7 @@ $timestamp = $nowDateTime->format('Ym');
 return [
     'prefix' => 'Argtyper' . $timestamp,
     'expose-constants' => ['#^SYMFONY\_[\p{L}_]+$#'],
-    'exclude-namespaces' => ['#^Rector#', '#^Symfony\\\\Polyfill#'],
+    'exclude-namespaces' => ['#^Rector#', '#^PhpParser#', '#^PHPStan#', '#^Symfony\\\\Polyfill#'],
     'exclude-files' => [
         // do not prefix "trigger_deprecation" from symfony - https://github.com/symfony/symfony/commit/0032b2a2893d3be592d4312b7b098fb9d71aca03
         // these paths are relative to this file location, so it should be in the root directory
