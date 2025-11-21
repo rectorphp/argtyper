c53ced862768d3c93aedddefb5594afc6397dcb4
Tomas Votruba <tomas.vot@gmail.com>
2025-11-21 09:25:07 +0000

    restore downgrade

--- bin/argtyper.php
+++ bin/argtyper.php
@@ -9,7 +9,19 @@ use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-require __DIR__ . '/../vendor/autoload.php';
+// installed as dependency
+if (file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
+    require_once __DIR__ . '/../../../../vendor/autoload.php';
+}
+
+if (file_exists(__DIR__ . '/../vendor/scoper-autoload.php')) {
+    // A. build downgraded package
+    require_once __DIR__ . '/../vendor/scoper-autoload.php';
+} else {
+    // B. local repository
+    require_once __DIR__ . '/../vendor/autoload.php';
+}
+
 
 $argTyperCommand = new ArgTyperCommand(
     new ProjectSourceDirFinder(),
