5fd194ac0cb1949aed70cd3fa71a4fcf8b866451
Tomas Votruba <tomas.vot@gmail.com>
2025-11-22 17:17:01 +0000

    not assert

--- src/Helpers/FilesLoader.php
+++ src/Helpers/FilesLoader.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace Rector\ArgTyper\Helpers;
 
-use Webmozart\Assert\Assert;
+// no extrnal services are allowed here, as this is used in PHPStan with project autoload, not local one
+// only PHPStan, PhpParser and Rector\ArgTyper is allowed here (including transitional dependencies)
 
 /**
  * @see https://jsonlines.org/
@@ -21,7 +22,6 @@ final class FilesLoader
             touch($filePath);
         }
 
-        Assert::fileExists($filePath);
         $line = json_encode($record, JSON_UNESCAPED_UNICODE) . PHP_EOL;
 
         // Append the line and lock the file to prevent race conditions
@@ -38,15 +38,19 @@ final class FilesLoader
             touch($filePath);
         }
 
-        Assert::fileExists($filePath);
-
         $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-        Assert::isArray($lines);
+        if (! is_array($lines)) {
+            throw new \RuntimeException(sprintf('Could not read file "%s".', $filePath));
+        }
 
         $records = [];
         foreach ($lines as $line) {
             $decoded = json_decode($line, true);
-            Assert::isArray($decoded);
+
+            if (! is_array($decoded)) {
+                throw new \RuntimeException(sprintf('Could not decode line "%s" in file "%s".', $line, $filePath));
+            }
+
             $records[] = $decoded;
         }
 
