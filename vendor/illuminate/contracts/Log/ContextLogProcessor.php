f23825ebf9e9f81b15ed5bc9926a2cfc5708a43f
GitHub Action <action@github.com>
2025-11-23 09:57:07 +0000

    release PHP 7.2 downgraded

---
+++ vendor/illuminate/contracts/Log/ContextLogProcessor.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Argtyper202511\Illuminate\Contracts\Log;
+
+use Argtyper202511\Monolog\Processor\ProcessorInterface;
+interface ContextLogProcessor extends ProcessorInterface
+{
+}
---
+++ vendor/rector/rector/vendor/doctrine/inflector/src/Rules/Substitution.php
@@ -0,0 +1,25 @@
+<?php
+
+declare (strict_types=1);
+namespace RectorPrefix202511\Doctrine\Inflector\Rules;
+
+final class Substitution
+{
+    /** @var Word */
+    private $from;
+    /** @var Word */
+    private $to;
+    public function __construct(\RectorPrefix202511\Doctrine\Inflector\Rules\Word $from, \RectorPrefix202511\Doctrine\Inflector\Rules\Word $to)
+    {
+        $this->from = $from;
+        $this->to = $to;
+    }
+    public function getFrom(): \RectorPrefix202511\Doctrine\Inflector\Rules\Word
+    {
+        return $this->from;
+    }
+    public function getTo(): \RectorPrefix202511\Doctrine\Inflector\Rules\Word
+    {
+        return $this->to;
+    }
+}
