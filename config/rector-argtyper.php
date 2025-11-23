da41503f9ff76a1ac62f47df8486f67d7517d8e6
Tomas Votruba <tomas.vot@gmail.com>
2025-11-23 14:15:35 +0000

    autoload

--- config/rector-argtyper.php
+++ config/rector-argtyper.php
@@ -3,16 +3,13 @@
 declare(strict_types=1);
 
 // to local classes in Rector autoload if not run from the same project
-require __DIR__ . '/bin/autoload.php';
+require __DIR__ . '/../bin/autoload.php';
 
 use Rector\ArgTyper\Rector\Rector\ClassMethod\AddClassMethodParamTypeRector;
 use Rector\ArgTyper\Rector\Rector\Function_\AddFunctionParamTypeRector;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-    ->withBootstrapFiles([
-        __DIR__ . '/../bin/autoload.php'
-    ])
     ->withSkip([
         // run only on source code
         'test',
