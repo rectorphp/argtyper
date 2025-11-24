8040d687a058c1ea01cefce6097809be4ce04751
Tomas Votruba <tomas.vot@gmail.com>
2025-11-24 14:29:31 +0000

    mini test

---
+++ tests/ValueObject/ProjectTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\ArgTyper\Tests\ValueObject;
+
+use PHPUnit\Framework\TestCase;
+use Rector\ArgTyper\ValueObject\Project;
+
+final class ProjectTest extends TestCase
+{
+    public function testMessage(): void
+    {
+        $this->expectExceptionMessage('The path "non-existing-path" is not a directory');
+
+        new Project('non-existing-path');
+    }
+
+    public function testMissingAutoload(): void
+    {
+        $this->expectExceptionMessage('Could not find "vendor/autoload.php" in the project. Make sure its dependencies are installed');
+
+        new Project(__DIR__);
+    }
+}
