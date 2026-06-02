21fc5d08275c717edd418f56cc2b4f90ec1dd171
Tomas Votruba <tomas.vot@gmail.com>
2026-06-02 22:42:29 +0200

    fix: Implicitly nullable parameters are deprecated are not parsed (fixes rectorphp/argtyper#2) (#11)
    * fix: keep nullable type on params to avoid dropping the ? (fixes #2)
    
    * style: apply automated fixers
    
    ---------
    
    Co-authored-by: fix-issue-bot <fix-issue-bot@localhost>

--- bin/autoload.php
+++ bin/autoload.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 // installed as dependency
 if (file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
     require_once __DIR__ . '/../../../../vendor/autoload.php';
