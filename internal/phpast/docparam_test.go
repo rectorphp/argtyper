59da93a7b36814d703e3822e039ff03e5b523da3
Tomas Votruba <tomas.vot@gmail.com>
2026-09-15 10:50:51 +0200

    Remove a param docblock made redundant by the added type (#26)

---
+++ internal/phpast/docparam_test.go
@@ -0,0 +1,59 @@
+package phpast_test
+
+import (
+	"testing"
+
+	"github.com/rectorphp/argtyper/internal/phpast"
+	"github.com/rectorphp/php-parser-in-go/pkg/ast"
+)
+
+func TestStripRedundantDocParams(t *testing.T) {
+	tests := []struct {
+		name  string
+		src   string
+		added map[string]string
+		want  string
+	}{
+		{
+			name:  "removes matching scalar tag and empties the doc",
+			src:   "<?php\nclass A {\n    /**\n     * @param string $v\n     */\n    public function m($v) {}\n}",
+			added: map[string]string{"v": "string"},
+			want:  "<?php\nclass A {\n    public function m($v) {}\n}",
+		},
+		{
+			name:  "matches object type regardless of leading backslash",
+			src:   "<?php\nclass A {\n    /**\n     * @param DateTime $d\n     */\n    public function m($d) {}\n}",
+			added: map[string]string{"d": "\\DateTime"},
+			want:  "<?php\nclass A {\n    public function m($d) {}\n}",
+		},
+		{
+			name:  "keeps a tag whose type differs from the added type",
+			src:   "<?php\nclass A {\n    /**\n     * @param mixed $v\n     */\n    public function m($v) {}\n}",
+			added: map[string]string{"v": "\\DateTime"},
+			want:  "<?php\nclass A {\n    /**\n     * @param mixed $v\n     */\n    public function m($v) {}\n}",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			root, err := phpast.Parse([]byte(test.src))
+			if err != nil {
+				t.Fatal(err)
+			}
+			var walk func(ast.Vertex)
+			walk = func(node ast.Vertex) {
+				if method, ok := node.(*ast.StmtClassMethod); ok {
+					phpast.StripRedundantDocParams(method, test.added)
+				}
+				for _, child := range phpast.Children(node) {
+					walk(child)
+				}
+			}
+			walk(root)
+
+			if got := phpast.Print(root); got != test.want {
+				t.Errorf("\n got: %q\nwant: %q", got, test.want)
+			}
+		})
+	}
+}
